dec135a934666c869db3955a175750e38e7d67d6
leodm5674 <98219987+leodm5674@users.noreply.github.com>
2026-09-03 08:23:20 -0300

    Add id field to Aluno struct in aluno.h

--- atividade/CRUD/aluno.h
+++ atividade/CRUD/aluno.h
@@ -1,10 +1,10 @@
 #ifndef ALUNO_H
 #define ALUNO_H
- 
 
 #define ARQUIVO "alunos.bin"
- 
+
 typedef struct {
+    int id;
     char nome[41];
     int matricula;
     float prova1;
@@ -13,3 +13,5 @@ typedef struct {
     int faltas;
     char situacao[10]; 
 } Aluno;
+
+#endif
